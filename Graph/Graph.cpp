7b845fb25b875357ec7e5db2f417d59eb469b59a
Gleb <gleb.homa6@gmail.com>
2026-04-11 16:58:48 +0800

    Fixed errors and Added missing methods

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,6 +1,6 @@
+#include "Graph.h"
 #include <iostream>
 #include <set>
-#include "Graph.h"
 using namespace std;
 
 void Graph::removeNode(Node* node) {
@@ -22,4 +22,28 @@ void Graph::addEdge(Node* begin, Node* end) {
 
 	begin->addNeighbour(end);
 	end->addNeighbour(begin);
+}
+
+void Graph::addNode(Node* node) {
+	nodes.insert(node);
+}
+
+void Graph::removeEdge(Node* begin, Node* end) {
+	if (nodes.find(begin) == nodes.end()) {
+		return;
+	}
+	if (nodes.find(end) == nodes.end()) {
+		return;
+	}
+
+	begin->removeNeighbour(end);
+	end->removeNeighbour(begin);
+}
+
+void Node::addNeighbour(Node* neighbour) {
+	neighbours.insert(neighbour);
+}
+
+void Node::removeNeighbour(Node* neighbour) {
+	neighbours.erase(neighbour);
 }
