00ec1d2d9ba014a5f081b2b25ca468d699838adc
Gleb <gleb.homa6@gmail.com>
2026-04-20 18:40:40 +0800

    Added func circle

--- Shape/Shape.h
+++ Shape/Shape.h
@@ -1,5 +1,4 @@
 #pragma once
-#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -9,4 +8,28 @@ public:
 	virtual double calc_area() = 0;
 	virtual double calcl_perimetr() = 0;
 	virtual void name() = 0;
+};
+
+class Circle : public Figure {
+	double center_x;
+	double center_y;
+	double radius;
+public:
+	Circle(double cx, double cy, double r) {
+		center_x = cx;
+		center_y = cy;
+		radius = r;
+	}
+
+	double calc_area() {
+		return PI * radius * radius;
+	}
+
+	double calc_perimetr() {
+		return 2 * PI * radius;
+	}
+
+	void name() {
+		cout << "Circle" << endl;
+	}
 };
