d2a4796c96874df3c18e2343c45d03dd79ff1a4e
Gleb <gleb.homa6@gmail.com>
2026-04-20 19:21:36 +0800

    Added test for Elipse

--- Shape/main.cpp
+++ Shape/main.cpp
@@ -11,5 +11,15 @@ int main() {
 	cout << "\n  Perimeter: " << c.calc_perimetr();
 	cout << "\n\n";
 
+	Elipse a(2, 5, 7, 6);
+	cout << "Testin Elipse (radius_a = 7, radius_b = 6:\n";
+	cout << " Name: ";
+	a.name();
+	cout << "\n Area: " << a.calc_area();
+	cout << "\n Perimeter: " << c.calc_perimetr();
+	cout << "\n\n";
 	return 0;
 }
+
+	return 0;
+}	
