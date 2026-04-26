236328cd4c581342e612e709eb64eb0407d7166b
Gleb <gleb.homa6@gmail.com>
2026-04-26 20:09:46 +0800

    Updated test

--- Shape/main.cpp
+++ Shape/main.cpp
@@ -11,15 +11,43 @@ int main() {
 	cout << "\n  Perimeter: " << c.calc_perimetr();
 	cout << "\n\n";
 
-	Elipse a(2, 5, 7, 6);
+	Elipse e(2, 5, 7, 6);
+
 	cout << "Testin Elipse (radius_a = 7, radius_b = 6:\n";
 	cout << " Name: ";
-	a.name();
-	cout << "\n Area: " << a.calc_area();
-	cout << "\n Perimeter: " << c.calc_perimetr();
+	e.name();
+	cout << "\n Area: " << e.calc_area();
+	cout << "\n Perimeter: " << e.calc_perimetr();
 	cout << "\n\n";
+	return 0;
+
+	Point<double> p1(0, 0);
+	Point<double> p2(3, 0);
+	Point<double> p3(0, 4);
+
+	Triangle tr(p1, p2, p3);
+	cout << "Testin:\n";
+	cout << " Name: ";
+	tr.name();
+	cout << "\n Area: " << tr.calc_area();
+	cout << "\n Perimeter: " << tr.calc_perimetr();
+	cout << "\n\n";
+
+	Point<double> quadPoints[] = {
+		Point<double>(0, 0),
+		Point<double>(4, 0),
+		Point<double>(4, 3),
+		Point<double>(0, 3)
+	};
+
+	Polygon poly(quadPoints, 4);
+	cout << "Testing Polygon:\n";
+	cout << " Name: ";
+	poly.name();
+	cout << "\n Area: " << poly.calc_area();
+	cout << "\n Perimeter: " << poly.calc_perimetr();
+	cout << "\n\n";
+
 	return 0;
 }
 
-	return 0;
-}	
