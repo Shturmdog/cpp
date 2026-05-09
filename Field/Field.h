4da6db32f71d944fc8add2cf031fc04f52f98c5e
Gleb <gleb.homa6@gmail.com>
2026-05-09 14:43:30 +0800

    Created class Field

---
+++ Field/Field.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Field {
+public:
+	Field(bool isRedFirst);
+	void clear(bool isRedFirst);
+	bool makeTurn(int column);
+	bool isWon(bool red) const;
+	bool isOver() const;
+	Cell getCell(int i, int j) const;
+	bool isRedTurnNow() const;
+	void print() const;
+	void printResult() const;
+};
