883daad2c5380be64143bcc59405c976ecf2eed8
Gleb <gleb.homa6@gmail.com>
2026-05-10 21:23:26 +0800

    Added func makeTurn and checkWinner

--- Field/Field.cpp
+++ Field/Field.cpp
@@ -15,3 +15,40 @@ void Field::clear(bool isRedFirst) {
 		}
 	}
 }
+
+bool Field::makeTurn(int column) {
+	if (winner != EMPTY || column < 1 || column > FIELD_WIDTH)
+		return false;
+
+	int i = column - 1;
+	for (int j = 0; i < FIELD_HEIGHT; j++)
+		if (cells[i][j] == EMPTY) {
+			cells[i][j] = isRedTurn ? RED : YELLOW;
+			checkWinner();
+			isRedTurn = !isRedTurn;
+			return true;
+		}
+	return false;
+}
+
+void Field::checkWinner() {
+	for (int i = 0; i < FIELD_WIDTH; i++)
+		for (int j = 0; j < FIELD_HEIGHT; j++) {
+			Cell start = cells[i][j];
+			if (start == EMPTY) continue;
+			for (int dir = 0; dir < DIR_NUMBER; dir++) {
+				int lenght = 0, iline = i, jline = j;
+				while (++lenght < WIN_LENGTH) {
+					iline += di[dir];
+					jline += dj[dir];
+					if (iline < 0 || iline >= FIELD_WIDTH || jline < 0 || jline >= FIELD_HEIGHT)
+						break;
+					if (cells[iline][jline] != start) break;
+				}
+				if (lenght == WIN_LENGTH) {
+					winner = start;
+					return;
+				}
+			}
+		}
+}
